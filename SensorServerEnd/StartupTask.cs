378102fab3efc7dbad13886daa8a9fd7276a4803
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-05-03 22:57:17 +1200

    Added UWP SensorServer and UWP Client UI applications for HMI bit

---
+++ SensorServerEnd/StartupTask.cs
@@ -0,0 +1,124 @@
+﻿// BME280 sensor - Temperature, Humidity and Air pressure
+//  https://github.com/gloveboxes/Windows-IoT-Core-Driver-Library
+//
+// Need to add a NuGet reference to Units.net V3.34 @ April 2019
+namespace SensorServerEnd
+{
+	using System;
+	using System.Diagnostics;
+	using System.Threading;
+	using Windows.ApplicationModel.Background;
+	using Glovebox.IoT.Devices.Sensors;
+	using Windows.ApplicationModel.AppService;
+	using Windows.Foundation.Collections;
+
+	public sealed class StartupTask : IBackgroundTask
+	{
+		private AppServiceConnection connection = null;
+		private BackgroundTaskDeferral backgroundTaskDeferral = null;
+		private static BME280 bme280Sensor;
+		private static Timer bme280InputPollingTimer;
+		private readonly TimeSpan timerDue = new TimeSpan(0, 0, 10);
+		private readonly TimeSpan timerPeriod = new TimeSpan(0, 0, 30);
+		private static DateTime? lastUpdatedAtUTC = null;
+		private static double temperature;
+		private static double humidity;
+		private static double airPressure;
+
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+			backgroundTaskDeferral = taskInstance.GetDeferral();
+			taskInstance.Canceled += TaskInstance_Canceled;
+
+			//Print out the FamilyName
+			Debug.WriteLine(Windows.ApplicationModel.Package.Current.Id.FamilyName);
+
+			// Setup the BME280 sensor and timer
+			if (bme280Sensor == null)
+			{
+				try
+				{
+					bme280Sensor = new BME280(0x76);
+
+					bme280InputPollingTimer = new Timer(SensorUpdateTimerCallback, null, timerDue, timerPeriod);
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine(ex.Message);
+				}
+			}
+
+			var appServiceTrigger = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+			if (appServiceTrigger != null)
+			{
+				Debug.WriteLine("appServiceTrigger != null");
+
+				//Verify that the app service connection is requesting the "" that this class provides
+				if (appServiceTrigger.Name.Equals("SensorServerEnd"))
+				{
+					//Store the connection and subscribe to the "RequestRecieved" event to be notified when clients send messages
+					connection = appServiceTrigger.AppServiceConnection;
+					connection.RequestReceived += Connection_RequestReceived;
+				}
+				else
+				{
+					backgroundTaskDeferral.Complete();
+					Debug.WriteLine("5");
+				}
+			}
+			else
+			{
+				Debug.WriteLine("appServiceTrigger == null");
+			}
+		}
+
+		private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+		{
+			Debug.WriteLine("TaskInstance_Canceled");
+
+			if (backgroundTaskDeferral != null)
+			{
+				backgroundTaskDeferral.Complete();
+				backgroundTaskDeferral = null;
+			}
+		}
+
+		private void SensorUpdateTimerCallback(object state)
+		{
+			try
+			{
+				temperature = bme280Sensor.Temperature.DegreesCelsius;
+				humidity = bme280Sensor.Humidity;
+				airPressure = bme280Sensor.Pressure.Kilopascals;
+				lastUpdatedAtUTC = DateTime.UtcNow;
+
+				Debug.WriteLine($"{lastUpdatedAtUTC} Timer triggered " +
+					$"Temperature: {temperature}°C " +
+					$"Humidity: {humidity:0.00}% " +
+					$"Air pressure: {airPressure}KPa ");
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+		private async void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+		{
+			ValueSet message = new ValueSet();
+
+			Debug.WriteLine("Connection_RequestReceived");
+
+			if (lastUpdatedAtUTC.HasValue)
+			{
+				message.Add("lastUpdatedAtUTC", lastUpdatedAtUTC.Value.ToString());
+				message.Add("temperature", temperature);
+				message.Add("humidity", humidity);
+				message.Add("airPressure", airPressure);
+
+				await args.Request.SendResponseAsync(message);
+			}
+		}
+	}
+}
+
---
+++ UWPClientEnd/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPClientEnd
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		AppServiceConnection connection;
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private async void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			Refresh.IsEnabled = false;
+
+			connection = new AppServiceConnection();
+			connection.AppServiceName = "SensorServerEnd";
+			connection.PackageFamilyName = "SensorServerEnd-uwp_nmn3tag1rpsaw";
+			AppServiceConnectionStatus status = await connection.OpenAsync();
+
+			if (status == AppServiceConnectionStatus.Success)
+			{
+				Refresh.IsEnabled = true; 
+			}
+		}
+
+		private async void Refresh_Click(object sender, RoutedEventArgs e)
+		{
+			Measurements.Items.Clear();
+
+			var message = new ValueSet();
+			var response = await connection.SendMessageAsync(message);
+
+			if (response.Message.ContainsKey("lastUpdatedAtUTC"))
+			{
+				foreach (var value in response.Message)
+				{
+					Measurements.Items.Add($"{value.Key}:{value.Value} ");
+				}
+			}
+		}
+	}
+}
